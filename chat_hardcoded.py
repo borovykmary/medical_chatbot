529390dc9d15a6ad3d4fba1a5fe1987bc02ff15a
Michał Skrzypczyński <276157@student.pwr.edu.pl>
2024-01-17 14:36:01 +0100

    added chat_hardcoded.py, fought with convert_to_pdf.py

---
+++ chat_hardcoded.py
@@ -0,0 +1,6 @@
+greetings = "Greetings patient, welcome to the queue of our clinic. Could you please share your name?"
+input_name = ""
+ask_for_symptoms = "What brings you here? What are your symptoms?"
+input_symptoms = ""
+calm = "It might be though - waiting, maybe chatting with our bot will make it more pleasant."
+answers = [input_name, input_symptoms]
--- convert_to_pdf.py
+++ convert_to_pdf.py
@@ -1,14 +1,12 @@
 # https://jeltef.github.io/PyLaTeX/current/examples/full.html
+from datetime import date
 
 from pylatex import Document, Section, Subsection
 
 if __name__ == '__main__':
-
-    geometry_options = {"tmargin": "1cm", "lmargin": "10cm"}
+    # def convert_to_pdf(answer: list[String]):
+    geometry_options = {"tmargin": "2cm", "lmargin": "2cm"}
+    datetime = str(date.today().strftime("%d/%m/%Y"))
     doc = Document(geometry_options=geometry_options)
-    with doc.create(Section('Patient\'s data')):
-        doc.append('Name: ')
-        doc.append('\nSymptoms: ')
-        doc.append('\nAll')
+    doc.create(Section(f'Patient\'s data {datetime}'))
     doc.generate_pdf('full', clean_tex=False, compiler='pdflatex')
-
